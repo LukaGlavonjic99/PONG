737f2489f817b88ccc343bd69ae3650c0006fca8
Luka <144832285+LukaGlavonjic99@users.noreply.github.com>
2024-06-18 12:39:35 +0200

    Create DesniReket.cs

---
+++ DesniReket.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+
+public class DesniReket : Reket
+{
+
+
+
+    public Rigidbody2D ball; 
+
+    private void FixedUpdate()
+    {
+        if (this.ball.velocity.x > 0.0f)
+        {
+            if (this.ball.position.y > this.transform.position.y)
+            {
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            }
+            else if (this.ball.position.y < this.transform.position.y)
+            {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            }
+        }
+        else
+        {
+            if (this.transform.position.y > 0.0f)
+            {
+                _rigidbody.AddForce(Vector2.down * this.speed);
+            }
+            else if (this.transform.position.y < 0.0f)
+            {
+                _rigidbody.AddForce(Vector2.up * this.speed);
+            }
+        }
+    }
+
+}
